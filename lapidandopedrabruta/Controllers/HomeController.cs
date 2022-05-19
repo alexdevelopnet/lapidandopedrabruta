77692a26844b53eee5b11140c13ba1bfb386127d
alexf <alexf@ANFSUSAX>
2022-05-19 03:51:51 -0300

    Add project files.

---
+++ lapidandopedrabruta/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lapidandopedrabruta.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Tratamentos()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+    }
+}
---
+++ lapidandopedrabruta/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lapidandopedrabruta.MvcApplication" Language="C#" %>
